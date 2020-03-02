13d400939f00b1547563bd85eb1dddfb07a38961
nighma <nighma@gmail.com>
2020-03-02 11:38:49 -0500

    p5> base done

--- p5/packages/app/src/index.ts
+++ p5/packages/app/src/index.ts
@@ -1,7 +1,5 @@
 import { APP_NAME } from '@my-project-boilerplate/server'
 
-// console.log('\nApplication name: ' + APP_NAME + '\n')
-
 export const getAppName = (): string => {
   return APP_NAME
 }
